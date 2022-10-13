f1efe69f96212c835ecddba93cda77897fd86033
David Marques <davidmarquesx@bol.com.br>
2022-10-12 21:51:11 -0300

    Add files via upload

---
+++ assets/include/trabalhos.php
@@ -0,0 +1,267 @@
+<div class="container">
+    <div class="btn prev align-center"><i class="fas fa-chevron-left" style="display: none"></i></div> 
+    <div class="btn next  align-center"><i class="fas fa-chevron-right"></i></div>
+    <div class="row">
+
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img12.jpg" class="vertical">
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td><img src="assets/img/clientes/andrea-bilinski.jpg"></td>
+                        <td> <h4>CLIENTE</h4> <p>Andrea Bilinski</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Landing page entendendo os homens de Abbrasil</p>  </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>              
+                             <p class="stars"></p> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+         </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img1.jpg">
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td><img src="https://workana.s3.amazonaws.com/logos/67/83d7d1a55f11e8819ffd212149f898/1602256.jpg"></td>
+                        <td> <h4>CLIENTE</h4> <p>Rodrigo Coifman</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Programador Frontend para Website</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>              
+                             <p>"Muito bom profissional! Me atendeu no tempo recorde."</p> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img2.jpg">
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td><img src="assets/img/clientes/samuel-santos.jpg"></td>
+                        <td> <h4>CLIENTE</h4> <p>Samuel Santos</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Design de Loja Virtual na plataforma Tray</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>              
+                             <p>"O David cumpriu o que foi solicitado dentro do projeto. Recomendo positivamente seus serviços. Voltaria a solicitar seus serviços."</p> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div> 
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img3.jpg">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://workana.s3.amazonaws.com/logos/bb/329371c42811e88874759c6d341b8c/1706436.jpg"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Michel Caldeira</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Design de banners criar banners novos do zero</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>               
+                             <p>"Ótimo profissional e espero fazer mais trabalhos com ele"</p> 
+                        </td>
+                    </tr>
+                </table> 
+            </div> 
+        </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img4.jpg">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://workana.s3.amazonaws.com/logos/43/0885b0f32d11e898389dd5f9f544c6/Sem-Titulo-2.jpg" onerror="this.src='assets/img/clientes/default-logo.jpg'"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Lucas Schievenin</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Shopify / Products per row + product page thumbnails + problemas com hover</p> </td>
+                    </tr>
+                    <tr class="comments">                
+                        <td> <i class="fas fa-comment"></i>
+                             <p>"Excelente profissional, realizou todo o trabalho com muita paciência e tive muita facilidade para me comunicar com o mesmo, responde rápido e claramente entende do assunto."</p> 
+                        </td>
+                    </tr>
+                </table> 
+            </div> 
+        </div>     
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img5.jpg">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://workana.s3.amazonaws.com/logos/d7/b2ed607f7c11e8b1ec65de1744326c/903822.png"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Nickolas Sertek</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Personalização de um template na plataforma Tray Commerce</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>
+                             <p>"Profissional muito bom! paciente ! resolveu bem rápido !"</p> 
+                        </td>
+                    </tr> 
+                </table>
+            </div>
+        </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img6.jpg">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <!-- <img src="https://wkncdn.com/newx/assets/build/img/logos/default_logo_3-normal.51b3f510c.jpg"> -->
+                            <img src="assets/img/clientes/robinson-valezin.jpg">
+                        </td>
+                        <td> <h4>CLIENTE</h4> <p>Robinson Valezin</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Site novo em word press com ranqueamento na primeira pagina do google por busca organica - usar template nova</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>
+                             <p>"excelente profissional. vou contratar novamente."</p> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div> 
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img7.jpg" class="vertical">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://workana.s3.amazonaws.com/logos/13/ee27ce3aa71036fac50729de574564/250598_313219212104803_83069101_n.png"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Dora Almeida</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Criação arte para Email Marketing em Html</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i> 
+                             <p class="stars"></p> 
+                         </td>
+                    </tr>
+                </table> 
+            </div>
+        </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img8.png">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://cf.workana.com/logos/f3/12231c8c494a088ca3417a8bd773a1/24528.jpg"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Monise Machado</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Corrigir Layout Site Responsivo</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>
+                             <p>"O David estendeu consideravelmente o prazo do projeto, porém, esteve disponível até a conclusão, e ficou satisfatório. Alguns pequenos ajustes que ele ainda precisa fazer, mas no geral ficou muito bom, e eu pretendo fazer mais projetos com ele. O projeto era realmente extenso. E a comunicação ocorreu sem problemas"</span></p> 
+                         </td>
+                    </tr>
+                </table> 
+            </div>
+        </div>        
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img9.jpg" class="vertical">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://wkncdn.com/newx/assets/build/img/logos/default_logo_4-normal.a7ae6b9ee.jpg"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Ana Castro</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Panfleto Para Hospital Veterinário</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>
+                             <p class="stars"></p> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="testimonial">
+            <div class="card-image col-md-6">
+                <img src="assets/img/projetos/img10.png">                  
+            </div>
+            <div class="card-text col-md-6">
+                <table>
+                    <tr class="client">
+                        <td> <img src="https://wkncdn.com/newx/assets/build/img/logos/default_logo_2-normal.1b701c523.jpg"> </td>
+                        <td> <h4>CLIENTE</h4> <p>Marcos</p> </td>
+                    </tr>
+                    <tr class="project">
+                        <td> <i class="fas fa-chalkboard"></i> </td>
+                        <td> <h4>PROJETO</h4> <p>Apresentação Power Point Animada</p> </td>
+                    </tr>
+                    <tr class="comments">    
+                        <td> <i class="fas fa-comment"></i>
+                             <p>"Rápida, prática, simples e direta!"</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
+    
+<script>
+    $('section#trabalhos .btn.next i').click(function(){
+        $('section#trabalhos .btn.prev i').fadeIn(); 
+        $('.testimonial:first-child').insertAfter('.testimonial:last-child');
+    });
+
+    $('section#trabalhos .btn.prev i').click(function(){
+        $('.testimonial:last-child').insertBefore('.testimonial:first-child');
+    });
+
+    var Vstars = '<i class="fas fa-star"></i>';
+    var star = document.querySelectorAll('.testimonial p.stars');
+    for(i=0; i < star.length; i++) { star[i].innerHTML = Vstars.repeat(5);  }
+</script>
