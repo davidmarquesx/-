d86a16e130f9077715c55fa60f5b9ef1830905b8
David Marques <davidmarquesx@bol.com.br>
2022-10-16 15:54:29 -0300

    Add files via upload

---
+++ assets/include/home.php
@@ -0,0 +1,15 @@
+<div class="container">
+	<div class="row">
+        <div class="main-image col-md-6">
+            <img src="assets/img/base/me.png" /> 
+        </div>
+        <div class="main-text col-md-6">
+            <div id="info">
+                <p>Olá! Sou</p>
+                <h1>David <br>Marques</h1>
+                <h2>Designer e front-end developer, focado em desenvolvimento de <strong> projetos gráficos</strong> e <strong>digitais</strong>.</h2>
+                <h3>Seja bem-vindo!</h3>
+            </div>
+        </div>
+    </div>
+</div>
