3994d22157be8cdc876dbf87555cb17abdd4f9e6
David Marques <davidmarquesx@bol.com.br>
2022-10-19 14:22:46 -0300

    Add files via upload

---
+++ assets/include/footer.php
@@ -0,0 +1,36 @@
+
+	<div class="row">
+		<div id="contact" class="col-md-6">
+		  <span>Contato:</span>
+		  <br>
+			<a href="tel:5521976697461" target="_blank"> 
+			  <i class="icon fa fa-sharp fa-solid fa-phone"></i>
+			</a>
+			<span>21 97669-7461</span>
+			<br>
+			<a href="mailto:davidmarquesx@bol.com.br" target="_blank">
+				<i class="icon fas fa-envelope"></i> 
+			</a>
+			<span>davidmarquesx@bol.com.br</span>
+		</div>
+		<div id="social" class="col-md-6 auto-middle">
+		<span>Seguir</span>
+		  <a href="https://api.whatsapp.com/send?l=pt&phone=5521976697461" target="_blank"> 
+		   	<i class="icon fab fa-whatsapp"></i> 
+			</a>
+			<a href="https://www.linkedin.com/in/davidmarquesx/" target="_blank"> 
+			  <i class="icon fab fa-brands fa-linkedin"></i>
+			</a>
+			<a href="https://www.instagram.com/accounts/login/" target="_blank">
+				<i class="icon fab fa-brands fa-instagram"></i>
+			</a>		
+			<a href="https://github.com/davidmarquesx/source" target="_blank">
+			  <i class="icon fab fa-brands fa-github"></i>
+			</a>
+		</div>
+	</div>
+	<div class="row">  
+		<div id="copyright" class="col-md-12 align-center">
+		  <span> ©David Marques 2014 </span>
+		</div>	
+	</div>
