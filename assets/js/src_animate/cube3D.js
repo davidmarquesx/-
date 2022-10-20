a0835adbdb01288ce5e84ad98f4861a10538f270
David Marques <davidmarquesx@bol.com.br>
2022-10-20 02:27:38 -0300

    Update cube3D.js

--- assets/js/src_animate/cube3D.js
+++ assets/js/src_animate/cube3D.js
@@ -1,14 +1,16 @@
 
 var cube = document.querySelector('#cube3D #element');
 
+cube.style.transform = 'rotateX(-22deg) rotateY(-45deg)';
+
 document.querySelector('#cube3D #controller .btn-left').onclick = function(){
     cube.style.transform += 'rotateY(-10deg) rotateX(-1deg)';
 }
 document.querySelector('#cube3D #controller .btn-up').onclick = function(){
-    cube.style.transform += 'rotateX(10deg)';
+    cube.style.transform += 'rotateX(10deg) rotateY(-1deg)';
 }
 document.querySelector('#cube3D #controller .btn-down').onclick = function(){
-    cube.style.transform += 'rotateX(-10deg)';
+    cube.style.transform += 'rotateX(-10deg) rotateY(1deg)';
 }
 document.querySelector('#cube3D #controller .btn-right').onclick = function(){
     cube.style.transform += 'rotateY(10deg) rotateX(-1deg)';
