d62dccf929e9ef47ab1959281ba8045cc29e26c9
David Marques <davidmarquesx@bol.com.br>
2022-10-27 06:02:06 -0300

    Update script.js

--- assets/js/theme/script.js
+++ assets/js/theme/script.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     xhttp.open('GET', 'assets/include/home.php', true);
     xhttp.send();
 
-    setTimeout(function(){ $('#preloader').fadeOut(); }, 2300)
+    setTimeout(function(){ $('#preloader').fadeOut(); }, 2000)
 
     setTimeout(function(){
         $('section#sobre').load('assets/include/sobre.php');
@@ -22,7 +22,7 @@ $(document).ready(function(){
         $('section#animacoes').load('assets/include/animacoes.php');
         $('footer').load('assets/include/footer.php');
         $('#preloader').remove();
-    }, 3000)
+    }, 2500)
 
 
 
