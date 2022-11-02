69430b7e6642a4cf1beff6a33116c1c5eb90e9d8
David Marques <davidmarquesx@bol.com.br>
2022-11-02 17:20:19 -0300

    Update footer.php

--- assets/include/footer.php
+++ assets/include/footer.php
@@ -14,18 +14,23 @@
 			<span>davidmarquesx@bol.com.br</span>
 		</div>
 		<div id="social" class="col-md-6 auto-middle">
-		<span>Seguir</span>
-		  <a href="https://api.whatsapp.com/send?l=pt&phone=5521976697461" target="_blank"> 
-		   	<i class="icon fab fa-whatsapp"></i> 
+			<span>Seguir</span>
+		  	<a href="https://api.whatsapp.com/send?l=pt&phone=5521976697461" target="_blank"> 
+		   		<i class="icon fab fa-whatsapp"></i> 
 			</a>
 			<a href="https://www.linkedin.com/in/davidmarquesx/" target="_blank"> 
-			  <i class="icon fab fa-brands fa-linkedin"></i>
+			 	<i class="icon fab fa-brands fa-linkedin"></i>
 			</a>
-			<a href="https://www.instagram.com/accounts/login/" target="_blank">
+			<!-- <a href="https://www.instagram.com/accounts/login/" target="_blank">
 				<i class="icon fab fa-brands fa-instagram"></i>
-			</a>		
+			</a> -->		
 			<a href="https://github.com/davidmarquesx/source" target="_blank">
-			  <i class="icon fab fa-brands fa-github"></i>
+				<i class="icon fab fa-brands fa-github"></i>
+			</a>
+			<a href="https://www.workana.com/freelancer/ec5a7e4db96c98241739c7c7d5af9e89" target="_blank">
+				<i class="icon fab fa-sharp fa-solid fa-o">
+					<img src="https://wkncdn.com/newx/assets/build/img/logos/mobile_logo.16f3a81b5.svg" alt="Workana - Acesse os melhores talentos da América Latina" style="width:25px; height:auto; margin:-5px 0; filter:grayscale(1) brightness(1.5);">
+				</i>
 			</a>
 		</div>
 	</div>
