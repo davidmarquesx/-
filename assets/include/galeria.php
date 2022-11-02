24f38906ece576ac39738a1a509e19a0e9417383
David Marques <davidmarquesx@bol.com.br>
2022-11-02 17:16:06 -0300

    Update galeria.php

--- assets/include/galeria.php
+++ assets/include/galeria.php
@@ -5,14 +5,14 @@
             <img src="assets/img/galeria/lina_bosque.jpg">
             <img src="assets/img/galeria/vetor_fireworks.jpg">
             <img src="assets/img/galeria/sandy.gif" class="gif">
-            <img src="assets/img/galeria/alice.jpg">
+            <img src="assets/img/galeria/alice.jpg" class="vertical" style="border:0">
             <img src="assets/img/galeria/immortal.gif" class="gif">
             <img src="assets/img/galeria/5bonecas.jpg">
         </div>
         <div class="grid-column">
             <img src="assets/img/galeria/anjo.jpg">
             <img src="assets/img/galeria/peninha.jpg">
-            <img src="assets/img/galeria/montagem_Sd_Ryan.jpg">
+            <img src="assets/img/galeria/montagem_Sd_Ryan.jpg" class="vertical">
             <img src="assets/img/galeria/gelo.jpg">
             <img src="assets/img/galeria/maquiagem.jpg">
             <img src="assets/img/galeria/briga.jpg">
@@ -23,19 +23,21 @@
             <img src="assets/img/galeria/bosque.jpg">
             <img src="assets/img/galeria/entrada.gif">
             <img src="assets/img/galeria/lara_croft.jpg">  
-            <img src="assets/img/galeria/boneca_fone.jpg">
+            <img src="assets/img/galeria/boneca_fone.jpg" class="vertical">
         </div>
         <div class="grid-column">
             <img src="assets/img/galeria/woman_flame.jpg">
             <img src="assets/img/galeria/spacemedia.jpg">
             <img src="assets/img/galeria/jaqueline.gif" class="gif">
-            <img src="assets/img/galeria/cartaz_infancia.jpg">
+            <img src="assets/img/galeria/cartaz_infancia.jpg" class="vertical">
             <img src="assets/img/galeria/music_works.gif">               
             <img src="assets/img/galeria/obscure.gif">                 
         </div>         
     </div>
     <div class="readmore auto-center">Ver Mais...</div>     
 </div>
+<div id="lightbox" class="auto-middle"></div>
+
 <!--<div class="overlay"><i class="far fa-times-circle"></i></div>-->
 
 <script type="text/javascript">
@@ -52,6 +54,17 @@
             }
         }    
         resize(elm); elm.addListener(resize)     
+    })
+
+    $('section#galeria img').click(function(){
+        $('#lightbox').append($(this).clone()).addClass('fadeIn');
+    })
+    $('#lightbox').click(function(){
+        $(this).addClass('fadeOut');
+        setTimeout(function(){
+            $('#lightbox').removeClass('fadeIn fadeOut'); $('#lightbox img').remove(); 
+        }, 1200)
+        
     })
 </script>
 
