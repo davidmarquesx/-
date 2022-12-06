763f265bfbd09170079371f0a4f20e4448d846b3
David Marques <davidmarquesx@bol.com.br>
2022-12-05 22:16:09 -0300

    Update animacoes.php

--- assets/include/animacoes.php
+++ assets/include/animacoes.php
@@ -107,7 +107,7 @@
         </div>       
         <div class="col-md-4">
             <div id="frog" class="source-animate">
-               <!-- <img src="assets/img/frog/elm-normal.png" id="image" /> -->
+                 <img src="assets/img/frog/elm-normal.png" id="image" />
                  <div id="avatar" class="auto-center">
                 <!-- <img src="assets/img/frog/elm-normal.png"> -->
                 </div> 
