cc78a0533d0c9f1c29d55862c435632cd6e2c2aa
David Marques <davidmarquesx@bol.com.br>
2024-02-14 15:31:27 -0300

    Update aquamask.js

--- assets/js/src_animate/aquamask.js
+++ assets/js/src_animate/aquamask.js
@@ -1,24 +1,22 @@
-
 var imageElement = document.querySelector('#aquamask svg:first-child');
-var maskedElement = document.querySelector('#aquamask #mask-circle');
+var maskedElement = document.querySelector('#aquamask #cursor #mask-circle');
 var svgPoint = imageElement.createSVGPoint();
 
-        function cursorPoint(aqua, svg) {
-            svgPoint.x = aqua.clientX;
-            svgPoint.y = aqua.clientY;
-            return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
-        }
-        function update(svgCoords) {
-            maskedElement.setAttribute('cx', svgCoords.x);
-            maskedElement.setAttribute('cy', svgCoords.y);
-        }
-        document.querySelector('#aquamask').addEventListener('pointermove', function(aqua) {
-            $(maskedElement).removeClass('aquamaskMove'); $('#aquamask svg:nth-child(2)').css('opacity','1').addClass('bubble');
-            update(cursorPoint(aqua, imageElement));   
+    function cursorPoint(aqua, svg) {
+        svgPoint.x = aqua.clientX;
+        svgPoint.y = aqua.clientY;
+        return svgPoint.matrixTransform(svg.getScreenCTM().inverse());
+    }
+    function update(svgCoords) {
+        maskedElement.setAttribute('cx', svgCoords.x);
+        maskedElement.setAttribute('cy', svgCoords.y);
+    }
+    document.querySelector('#aquamask').addEventListener('pointermove', function(aqua) {
+        $(maskedElement).removeClass('aquamaskMove'); $('#aquamask svg:nth-child(2)').css('opacity','1');
+        update(cursorPoint(aqua, imageElement));
+        setTimeout(maskStop, 3000); 
+    }, false);
 
-            setTimeout(maskStop, 1000); 
-        }, false);
-
-        function maskStop() {
-            $('#aquamask svg:nth-child(2)').removeClass('bubble');
-        }
+    function maskStop() {          
+        $(maskedElement).addClass('aquamaskMove');
+    }
